e5cf10f589b84d487eb9e570d72f75d113931b6c
Andrew Diaz <andrewdiazn20@gmail.com>
2024-05-09 15:20:16 -0700

    finalpush

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,15 @@
-//? i dont know how to make a search?//
 // Your Code Here
+
+let response = await fetch('http://localhost:3001/listBooks', {
+    method:"PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "id":3,
+        "title":"Legends of Arathrae",
+    }),
+});
+
+let updatedBook = await response.json();
+console.log(updatedBook);
